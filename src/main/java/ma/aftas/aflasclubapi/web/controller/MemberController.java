72ab5d6fccaf2350c30f159aec4079cfe66758bf
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-12 18:57:49 +0100

    [ FEATURE ][ AFTS-8 ][ recherche d’un adhérent par numéro, nom, ou prénom  ] {{base_path}}/members/search?familyName=Lehner&name=ELMAHDI&num=HH1222-ELMAHDI

--- src/main/java/ma/aftas/aflasclubapi/web/controller/MemberController.java
+++ src/main/java/ma/aftas/aflasclubapi/web/controller/MemberController.java
@@ -1,24 +1,29 @@
 package ma.aftas.aflasclubapi.web.controller;
 
 
+import lombok.extern.log4j.Log4j2;
 import ma.aftas.aflasclubapi.dto.MemberDto;
 import ma.aftas.aflasclubapi.dto.MemberResponseDto;
 import ma.aftas.aflasclubapi.web.service.MemberService;
+import ma.aftas.aflasclubapi.web.service.impl.CompetitionServiceImpl;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/members")
+@Log4j2
 public class MemberController {
 
     //: Inscription d’un membre dans une compétition
     // (Chercher le membre et l'insérer s’il n’existe pas)
     private MemberService memberService ;
+
+    Logger logger = org.slf4j.LoggerFactory.getLogger(MemberController.class);
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
@@ -29,6 +34,16 @@ public class MemberController {
         return new ResponseEntity<>(this.memberService.addMember(memberDto),
                 HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/search")
+    //TODO :  recherche d’un adhérent par numéro, nom, ou prénom .
+    public ResponseEntity<MemberDto> findUserByDifferenceParams(@RequestParam Map<String,String> queryParams,
+                                                                 @RequestHeader("Authorization") String token
+                                                                 ){
+
+        log.info("Controller Request Params : "+queryParams.toString());
+        return ResponseEntity.ok(this.memberService.findMemberByMoreThanParam(queryParams));
     }
 
 
